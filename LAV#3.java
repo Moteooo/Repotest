0d0eea630a7de0c4b4e408667748f74e8fa0398c
Gansukh <ggansukh2001@gmail.com>
2022-03-24 15:05:37 +0800

    Update LAV#3.java
    //

--- LAV#3.java
+++ LAV#3.java
@@ -58,7 +58,7 @@ public class StringTask2 {
 		System.out.println(expression + " = " + result);
 	}
 	
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
